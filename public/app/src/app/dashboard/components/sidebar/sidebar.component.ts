90fc7a28974d5d6717428406cf071d05f86d83a7
Earl Hans Genoso <earlhansg@gmail.com>
2019-02-28 02:43:42 +0800

    #7 Sidebar Component (Part-1)

--- public/app/src/app/dashboard/components/sidebar/sidebar.component.ts
+++ public/app/src/app/dashboard/components/sidebar/sidebar.component.ts
@@ -3,40 +3,8 @@ import { faFile } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sidebar',
-  styleUrls: ['./sidebar.component.css'],
-  template: `
-    <div class="col-md-3">
-        <div id="sidebar">
-            <div class="sidebar-logo">
-                <div>
-                    <fa-icon [icon]="faFile"></fa-icon>
-                <div>
-            </div>
-            <div class="sidebar-menu">
-                <ul>
-                    <li>
-                    <a href="#">IMAGES</a>
-                    </li>
-                    <li>
-                    <a href="#">IMPORTANT</a>
-                    </li>
-                    <li>
-                    <a href="#">ARCHIVES</a>
-                    </li>
-                    <li>
-                    <a href="#">SNOOZE</a>
-                    </li>
-                    <li>
-                    <a href="#">DELETED</a>
-                    </li>
-                    <li>
-                    <a href="#">FRIENDS</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-  `
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
 
